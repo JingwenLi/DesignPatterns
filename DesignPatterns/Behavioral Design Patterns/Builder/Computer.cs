e2e546d9c60f65408a54988fec935c972efea481
nancy.li <nancy.li@wisetechglobal.com>
2022-10-16 18:08:20 +0800

    U

---
+++ DesignPatterns/Behavioral Design Patterns/Builder/Computer.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Builder
+{
+	public class Computer
+    {
+        public string CPU { get; set; }
+        public string Memory { get; set; }
+        public string Disk { get; set; }
+        public string GraphicsCard { get; set; }
+        public string MainBoard { get; set; }
+    }
+}
---
+++ DesignPatterns/Behavioral Design Patterns/Builder/Director.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.Builder
+{
+	public class Director
+    {
+        private Builder builder = null;
+
+        public Director(Builder builder)
+        {
+            this.builder = builder;
+        }
+
+        public Computer AssembleComputer()
+        {
+            builder.BuildCPU();
+            builder.BuildDisk();
+            builder.BuildGraphicsCard();
+            builder.BuildMainBoard();
+            builder.BuildMemory();
+            return builder.CreateComputer();
+        }
+    }
+}

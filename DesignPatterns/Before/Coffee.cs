050e3ed6d692038a668fdfea41c1255cb585669f
nancy.li <nancy.li@wisetechglobal.com>
2022-10-16 16:34:36 +0800

    U

---
+++ DesignPatterns/Before/Coffee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesignPatterns.Before
+{
+    public abstract class Coffee
+    {
+        public abstract string GetName();
+
+        public void AddSugar()
+        {
+            Console.WriteLine("加糖");
+        }
+
+        public void AddMilk()
+        {
+            Console.WriteLine("加奶");
+        }
+    }
+
+    public class LatterCoffee : Coffee
+    {
+        public override string GetName()
+        {
+            return "拿铁咖啡";
+        }
+    }
+
+    public class AmericanCoffee : Coffee
+    {
+        public override string GetName()
+        {
+            return "美式咖啡";
+        }
+    }
+}

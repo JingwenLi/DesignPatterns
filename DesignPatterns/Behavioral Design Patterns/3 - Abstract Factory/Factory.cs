e6ec893bd7b5fc6b037f88b6fff5ab980a233e0a
nancy.li <nancy.li@wisetechglobal.com>
2022-10-16 17:59:38 +0800

    U

--- DesignPatterns/Behavioral Design Patterns/3 - Abstract Factory/Factory.cs
+++ DesignPatterns/Behavioral Design Patterns/3 - Abstract Factory/Factory.cs
@@ -1,6 +1,8 @@
 ﻿namespace DesignPatterns.AbstractFactory
 {
-	public interface IFactory
+    //有的时候，我们要创建一些相关联的或依赖的一族对象，这个时候可以把创建这些对象用抽象工厂来实现，而不是用工厂方法模式来实现，
+    //(如果用工厂方法模式来实现，一个抽象产品对应一个抽象工厂，那样的情况叫做“类型爆炸”)
+    public interface IFactory
     {
         MobilePhone CreateMobilePhone();
         Watch CreateWatch();

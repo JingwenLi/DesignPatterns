1ae95734e58165a7fe57ae5f43921ef740d73a3e
nancy.li <nancy.li@wisetechglobal.com>
2022-10-16 20:36:18 +0800

    U

--- DesignPatterns/Creational Design Patterns/6 - Singleton/InnerClassSignleton.cs
+++ DesignPatterns/Creational Design Patterns/6 - Singleton/InnerClassSignleton.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DesignPatterns.Creational_Design_Patterns._6___Singleton
+namespace DesignPatterns.Singleton
 {
-    class InnerClassSignleton
+	class InnerClassSignleton
     {
         private static class InnerClassHolder
         {

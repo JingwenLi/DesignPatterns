cfade447cd9b73a6cb8b419aea65e126d8d3fae3
nancy.li <nancy.li@wisetechglobal.com>
2022-10-16 20:39:05 +0800

    U

--- DesignPatterns/Creational Design Patterns/6 - Singleton/InnerClassSignleton.cs
+++ DesignPatterns/Creational Design Patterns/6 - Singleton/InnerClassSignleton.cs
@@ -2,22 +2,18 @@
 
 namespace DesignPatterns.Singleton
 {
-	class InnerClassSignleton
+    class InnerClassSignleton
     {
+        private InnerClassSignleton()
+        {
+        }
+        
         private static class InnerClassHolder
         {
             public static InnerClassSignleton instance = new InnerClassSignleton();
         }
 
-        private InnerClassSignleton()
-        {
-            if (InnerClassHolder.instance != null)
-            {
-                throw new Exception("单例模式不允许多个实例");
-            }
-        }
-
-        public static InnerClassSignleton getInstance()
+        public static InnerClassSignleton GetInstance()
         {
             return InnerClassHolder.instance;
         }

3a1bad5f6526611d19bbb692a8df8a752c7b7472
nancy.li <nancy.li@wisetechglobal.com>
2022-10-16 18:27:57 +0800

    U

---
+++ DesignPatterns.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesignPatterns.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesignPatterns.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4e3848d-7748-42e2-84eb-bdd9a79b7876")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DesignPatterns.Test/SimpleFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DesignPatterns.Test
+{
+	[TestClass]
+	public class SimpleFactoryTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}

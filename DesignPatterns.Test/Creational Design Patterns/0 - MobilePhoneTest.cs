a7705ca2ca15d5cced48199238b0736181f17b2a
nancy.li <nancy.li@wisetechglobal.com>
2022-10-18 18:41:26 +0800

    Update

---
+++ DesignPatterns.Test/Creational Design Patterns/0 - MobilePhoneTest.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.SimpleFactory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatterns
+{
+    public class MobilePhoneTest
+    {
+        [TestClass]
+        public class SimpleFactoryTest
+        {
+            [TestMethod]
+            public void TestCall()
+            {
+                new XiaoMiPhone().Call();
+                new HuaWeiPhone().Call();
+            }
+        }
+    }
+}

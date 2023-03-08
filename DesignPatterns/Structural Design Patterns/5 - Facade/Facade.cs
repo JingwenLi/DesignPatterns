814e7f00bcd6363b7f421a31b40c90cd846bc71f
nancy.li <nancy.li@wisetechglobal.com>
2023-03-08 15:27:17 +0800

    Update

---
+++ DesignPatterns/Structural Design Patterns/5 - Facade/Facade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DesignPatterns.Facade
+{
+	class Client
+	{
+		static void Main(string[] args)
+		{
+			var facade = new SystemFacade();
+			facade.SubmitOrder();
+		}
+	}
+
+	public class WarehouseSystem
+	{
+		public void Pack()
+		{
+			Console.WriteLine("Pack good");
+		}
+	}
+
+	public class LogisticsSystem
+	{
+		public void Delivery()
+		{
+			Console.WriteLine("Delivery goods");
+		}
+	}
+
+	public class SystemFacade
+	{
+		private WarehouseSystem warehouseSystem;
+		private LogisticsSystem logisticsSystem;
+
+		public SystemFacade()
+		{
+			warehouseSystem = new WarehouseSystem();
+			logisticsSystem = new LogisticsSystem();
+		}
+
+		public void SubmitOrder()
+		{
+			warehouseSystem.Pack();
+			logisticsSystem.Delivery();
+		}
+	}
+}

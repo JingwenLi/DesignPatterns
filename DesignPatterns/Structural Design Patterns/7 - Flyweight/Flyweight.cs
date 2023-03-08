aa0cc638c82528e9346c3780f142bd2d516f8cd3
nancy.li <nancy.li@wisetechglobal.com>
2023-03-08 15:52:31 +0800

    U

---
+++ DesignPatterns/Structural Design Patterns/7 - Flyweight/Flyweight.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Flyweight
+{
+	class Client
+	{
+		static void Main(string[] args)
+		{
+			var factory = new FlyweightFactory();
+
+			var flyweightA = factory.GetFlyweight("A");
+			flyweightA.Operation();
+
+			var flyweightB = factory.GetFlyweight("B");
+			flyweightB.Operation();
+
+			var flyweightC = factory.GetFlyweight("C");
+			flyweightC.Operation();
+
+			var flyweightD = factory.GetFlyweight("D");
+			flyweightD.Operation();
+		}
+	}
+
+	public abstract class Flyweight
+	{
+		public abstract void Operation();
+	}
+
+	public class ConcreteFlyweight : Flyweight
+	{
+		public override void Operation()
+		{
+			Console.WriteLine("");
+		}
+	}
+
+	class FlyweightFactory
+	{
+		public Dictionary<string, Flyweight> Flyweights = new Dictionary<string, Flyweight>();
+
+		public Flyweight GetFlyweight(string key)
+		{
+			if (Flyweights.ContainsKey(key))
+			{
+				return Flyweights[key];
+			}
+			else
+			{
+				Flyweights.Add(key, new ConcreteFlyweight());
+				return Flyweights[key];
+			}
+		}
+	}
+}

4b5986f1002b5992353e132534b74071046fedc0
nancy.li <nancy.li@wisetechglobal.com>
2023-03-19 14:08:36 +0800

    U

--- DesignPatterns/Structural Design Patterns/5 - Decorator/Decorator.cs
+++ DesignPatterns/Structural Design Patterns/5 - Decorator/Decorator.cs
@@ -2,28 +2,6 @@
 
 namespace DesignPatterns.Decorator
 {
-	public class Client
-	{
-		public static void Run()
-		{
-			var simple = new ConcreteComponent();
-			Console.WriteLine("I get a simple component:");
-			Console.WriteLine("RESULT: " + simple.Operation()); 
-			Console.WriteLine();
-
-			var decorator1 = new ConcreteDecoratorA(simple);
-			Console.WriteLine("I get a decorated component:");
-			Console.WriteLine("RESULT: " + decorator1.Operation());
-			Console.WriteLine();
-
-			// Note how decorators can wrap not only simple components but the other decorators as well.
-			var decorator2 = new ConcreteDecoratorB(decorator1);
-			Console.WriteLine("I get a decorated component:");
-			Console.WriteLine("RESULT: " + decorator2.Operation());
-			Console.WriteLine();
-		}
-	}
-
 	// The base Component interface defines operations that can be altered by decorators.
 	public abstract class Component
 	{
@@ -48,7 +26,7 @@ namespace DesignPatterns.Decorator
 			this.component = component;
 		}
 
-		// The Decorator delegates all work to the wrapped component.
+		// The Decorator delegates all operations to the wrapped component.
 		public override string Operation()
 		{
 			return component.Operation();
@@ -72,6 +50,22 @@ namespace DesignPatterns.Decorator
 		public override string Operation()
 		{
 			return $"ConcreteDecoratorB({base.Operation()})";
+		}
+	}
+
+	public class Client
+	{
+		public static void Run()
+		{
+			var simple = new ConcreteComponent();
+			Console.WriteLine("RESULT: " + simple.Operation());
+
+			var decorator1 = new ConcreteDecoratorA(simple);
+			Console.WriteLine("RESULT: " + decorator1.Operation());
+
+			// Note how decorators can wrap not only simple components but the other decorators as well.
+			var decorator2 = new ConcreteDecoratorB(decorator1);
+			Console.WriteLine("RESULT: " + decorator2.Operation());
 		}
 	}
 }

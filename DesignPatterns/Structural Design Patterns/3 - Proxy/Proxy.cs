99ebc881f1a89f4a3227fd163d6faf18cf64cb9b
nancy.li <nancy.li@wisetechglobal.com>
2023-03-19 10:38:49 +0800

    U

--- DesignPatterns/Structural Design Patterns/3 - Proxy/Proxy.cs
+++ DesignPatterns/Structural Design Patterns/3 - Proxy/Proxy.cs
@@ -2,27 +2,6 @@
 
 namespace DesignPatterns.Proxy
 {
-	public class Client
-	{
-		public static void Run()
-		{
-			Console.WriteLine("Client: Executing with a real subject:");
-			var realSubject = new RealSubject();
-			Execute(realSubject);
-
-			Console.WriteLine();
-
-			Console.WriteLine("Client: Executing with a proxy:");
-			Proxy proxy = new Proxy(realSubject);
-			Execute(proxy);
-		}
-
-		public static void Execute(ISubject subject)
-		{
-			subject.Request();
-		}
-	}
-
 	// The Subject interface declares common operations for both RealSubject and the Proxy.
 	// As long as the client works with RealSubject using this interface, you'll be able to pass in a proxy instead of a real subject.
 	public interface ISubject
@@ -69,6 +48,27 @@ namespace DesignPatterns.Proxy
 		public void LogAccess()
 		{
 			Console.WriteLine("Proxy: Logging request time.");
+		}
+	}
+
+	public class Client
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Client: Executing with a real subject:");
+			var realSubject = new RealSubject();
+			Execute(realSubject);
+
+			Console.WriteLine();
+
+			Console.WriteLine("Client: Executing with a proxy:");
+			Proxy proxy = new Proxy(realSubject);
+			Execute(proxy);
+		}
+
+		public static void Execute(ISubject subject)
+		{
+			subject.Request();
 		}
 	}
 }

9fe22a907d4dbd9afc5cf556872a68399d8ecc11
Nancy Li <nancy.li@wisetechglobal.com>
2023-07-03 12:59:18 +0800

    U

---
+++ DesignPatterns/Behavioral Design Patterns/Mediator/Component.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DesignPatterns.Mediator
+{
+	// The Base Component provides the basic functionality of storing a
+	// mediator's instance inside component objects.
+	class BaseComponent
+	{
+		protected IMediator _mediator;
+
+		public BaseComponent(IMediator mediator = null)
+		{
+			this._mediator = mediator;
+		}
+
+		public void SetMediator(IMediator mediator)
+		{
+			this._mediator = mediator;
+		}
+	}
+
+	// Concrete Components implement various functionality. They don't depend on
+	// other components. They also don't depend on any concrete mediator
+	// classes.
+	class Component1 : BaseComponent
+	{
+		public void DoA()
+		{
+			Console.WriteLine("Component 1 does A.");
+
+			this._mediator.Notify(this, "A");
+		}
+
+		public void DoB()
+		{
+			Console.WriteLine("Component 1 does B.");
+
+			this._mediator.Notify(this, "B");
+		}
+	}
+
+	class Component2 : BaseComponent
+	{
+		public void DoC()
+		{
+			Console.WriteLine("Component 2 does C.");
+
+			this._mediator.Notify(this, "C");
+		}
+
+		public void DoD()
+		{
+			Console.WriteLine("Component 2 does D.");
+
+			this._mediator.Notify(this, "D");
+		}
+	}
+}

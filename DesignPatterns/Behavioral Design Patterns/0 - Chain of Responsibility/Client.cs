790cb8e88a4076284e07d686d95659be89adb11d
Nancy Li <nancy.li@wisetechglobal.com>
2023-07-03 12:48:37 +0800

    U

---
+++ DesignPatterns/Behavioral Design Patterns/0 - Chain of Responsibility/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.ChainOfResponsibility
+{
+	internal class Client
+	{
+		public static void Run()
+		{
+			// The other part of the client code constructs the actual chain.
+			var monkey = new MonkeyHandler();
+			var squirrel = new SquirrelHandler();
+			var dog = new DogHandler();
+
+			monkey.SetNext(squirrel).SetNext(dog);
+
+			// The client should be able to send a request to any handler, not
+			// just the first one in the chain.
+			Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+			Run(monkey);
+			Console.WriteLine();
+
+			Console.WriteLine("Subchain: Squirrel > Dog\n");
+			Run(squirrel);
+		}
+
+		public static void Run(AbstractHandler handler)
+		{
+			foreach (var food in new List<string> { "Nut", "Banana", "Cup of coffee" })
+			{
+				Console.WriteLine($"Client: Who wants a {food}?");
+
+				var result = handler.Handle(food);
+
+				if (result != null)
+				{
+					Console.Write($"   {result}");
+				}
+				else
+				{
+					Console.WriteLine($"   {food} was left untouched.");
+				}
+			}
+		}
+	}
+}
---
+++ DesignPatterns/Behavioral Design Patterns/0 - Chain of Responsibility/Handler.cs
@@ -0,0 +1,82 @@
+﻿namespace DesignPatterns.ChainOfResponsibility
+{
+	public interface IHandler
+	{
+		IHandler SetNext(IHandler handler);
+
+		object Handle(object request);
+	}
+
+	// The default chaining behavior can be implemented inside a base handler class.
+	abstract class AbstractHandler : IHandler
+	{
+		private IHandler _nextHandler;
+
+		public IHandler SetNext(IHandler handler)
+		{
+			this._nextHandler = handler;
+
+			// Returning a handler from here will let us link handlers in a
+			// convenient way like this:
+			// monkey.SetNext(squirrel).SetNext(dog);
+			return handler;
+		}
+
+		public virtual object Handle(object request)
+		{
+			if (this._nextHandler != null)
+			{
+				return this._nextHandler.Handle(request);
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+
+	class MonkeyHandler : AbstractHandler
+	{
+		public override object Handle(object request)
+		{
+			if ((request as string) == "Banana")
+			{
+				return $"Monkey: I'll eat the {request.ToString()}.\n";
+			}
+			else
+			{
+				return base.Handle(request);
+			}
+		}
+	}
+
+	class SquirrelHandler : AbstractHandler
+	{
+		public override object Handle(object request)
+		{
+			if (request.ToString() == "Nut")
+			{
+				return $"Squirrel: I'll eat the {request.ToString()}.\n";
+			}
+			else
+			{
+				return base.Handle(request);
+			}
+		}
+	}
+
+	class DogHandler : AbstractHandler
+	{
+		public override object Handle(object request)
+		{
+			if (request.ToString() == "MeatBall")
+			{
+				return $"Dog: I'll eat the {request.ToString()}.\n";
+			}
+			else
+			{
+				return base.Handle(request);
+			}
+		}
+	}
+}

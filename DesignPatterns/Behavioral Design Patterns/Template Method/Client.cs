3793910cc61f9f959c5785a71f425207013bcffc
Nancy Li <nancy.li@wisetechglobal.com>
2023-07-03 13:17:20 +0800

    U

---
+++ DesignPatterns/Behavioral Design Patterns/Template Method/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DesignPatterns.TemplateMethod
+{
+	internal class Client
+	{
+
+		// The client code calls the template method to execute the algorithm.
+		// Client code does not have to know the concrete class of an object it
+		// works with, as long as it works with objects through the interface of
+		// their base class.
+		public static void ClientCode(AbstractClass abstractClass)
+		{
+			// ...
+			abstractClass.TemplateMethod();
+			// ...
+		}
+
+		public static void Run()
+		{
+			Console.WriteLine("Same client code can work with different subclasses:");
+
+			Client.ClientCode(new ConcreteClass1());
+
+			Console.Write("\n");
+
+			Console.WriteLine("Same client code can work with different subclasses:");
+			Client.ClientCode(new ConcreteClass2());
+		}
+	}
+}

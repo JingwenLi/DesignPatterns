0dabc45138a5ca198466e721fe9f6f3a75ee1a65
Nancy Li <nancy.li@wisetechglobal.com>
2023-07-03 13:07:22 +0800

    U

---
+++ DesignPatterns/Behavioral Design Patterns/Memento/Memento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Memento
+{
+	internal class Client
+	{
+		public static void Run()
+		{
+			// The client code.
+			Component1 component1 = new Component1();
+			Component2 component2 = new Component2();
+			new ConcreteMediator(component1, component2);
+
+			Console.WriteLine("Client triggers operation A.");
+			component1.DoA();
+
+			Console.WriteLine();
+
+			Console.WriteLine("Client triggers operation D.");
+			component2.DoD();
+		}
+	}
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,12 +3,48 @@ using System;
 
 namespace DesignPatterns
 {
-	internal class Program
+	// The Memento interface provides a way to retrieve the memento's metadata,
+	// such as creation date or name. However, it doesn't expose the
+	// Originator's state.
+	public interface IMemento
 	{
-		static void Main(string[] args)
+		string GetName();
+
+		string GetState();
+
+		DateTime GetDate();
+	}
+
+	// The Concrete Memento contains the infrastructure for storing the
+	// Originator's state.
+	class ConcreteMemento : IMemento
+	{
+		private string _state;
+
+		private DateTime _date;
+
+		public ConcreteMemento(string state)
 		{
-			//HungrySingleton.GetHI();
-			//InnerClassSignleton.GetHI();
+			this._state = state;
+			this._date = DateTime.Now;
+		}
+
+		// The Originator uses this method when restoring its state.
+		public string GetState()
+		{
+			return this._state;
+		}
+
+		// The rest of the methods are used by the Caretaker to display
+		// metadata.
+		public string GetName()
+		{
+			return $"{this._date} / ({this._state.Substring(0, 9)})...";
+		}
+
+		public DateTime GetDate()
+		{
+			return this._date;
 		}
 	}
 }

b7ffd11fa4b6e74e5aee05389c57f7b57ca89b53
nancy.li <nancy.li@wisetechglobal.com>
2023-09-23 13:13:45 +0800

    U

--- DesignPatterns/Behavioral Design Patterns/PublishSubscriber/Client.cs
+++ DesignPatterns/Behavioral Design Patterns/PublishSubscriber/Client.cs
@@ -4,14 +4,18 @@
 	{
 		public static void Run()
 		{
-			var message = new Message("Important Info");
+			var message1 = new Message("Important Info 1");
+			var message2 = new Message("Important Info 2");
+
 			var publisher = new Publisher();
+
 			var observerA = new ConcreteSubscriberA();
 			var observerB = new ConcreteSubscriberB();
 
 			publisher.Attach(observerA);
 			publisher.Attach(observerB);
-			publisher.PublishMessage(message);
+			publisher.PublishMessage(message1);
+			publisher.PublishMessage(message2);
 		}
 	}
 }
